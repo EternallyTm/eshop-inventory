55fd3f434369771b434c013ad1f508f5446f0de0
EternallyTm <1028106567@qq.com>
2019-03-01 14:18:18 +0800

    新建库存服务项目

---
+++ src/main/java/com/wangx/eshop/inventory/EshopInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.wangx.eshop.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopInventoryApplication.class, args);
+    }
+
+}
